87d5d467563c5733cbb2ce06006e69bc54c90fb8
stenman <roger.stenman@gmail.com>
2015-11-01 12:58:25 +0100

    added some schemas

---
+++ database/resources/populate1_config_data.sql
@@ -0,0 +1,2 @@
+insert into KEYSTORE.configuration (config_id, config_key, config_value) values (next value for CONFIGURATION_SEQUENCE, 'some.config.key', 'somevalue');
+
---
+++ database/resources/populate2_user_data.sql
@@ -0,0 +1,1 @@
+insert into KEYSTORE.credentials (user_id, email_id, first_name, last_name, password) values (next value for CONFIGURATION_SEQUENCE, 'Greg', 'greg@example.com', 'Greg', 'Gregson', '');
